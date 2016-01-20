780113e310fa577f19ca0a9e18f7179d964f9cfa
hereszix <zixerino@gmail.com>
2016-01-20 03:17:06 +0000

    created components directory and added post form to it

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,6 +10,7 @@ class PagesController < ApplicationController
     if(User.find_by_username(params[:id]))
       @username = params[:id]
       @posts = Post.all.where("user_id = ?", User.find_by_username(params[:id]).id)
+      @newPost = Post.new
     else
       redirect_to root_path, :notice=> "User not found!"
     end
