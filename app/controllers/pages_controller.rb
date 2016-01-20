60c2461c46bff4c6eb3202e61468cbe56cfcf903
hereszix <zixerino@gmail.com>
2016-01-20 00:58:55 +0000

    added the ability to display posts

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,16 +3,19 @@ class PagesController < ApplicationController
   end
 
   def home
+    @posts = Post.all
   end
 
   def profile
     if(User.find_by_username(params[:id]))
       @username = params[:id]
+      @posts = Post.all.where("user_id = ?", User.find_by_username(params[:id]).id)
     else
       redirect_to root_path, :notice=> "User not found!"
     end
   end
 
   def explore
+    @posts = Post.all
   end
 end
